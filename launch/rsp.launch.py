5cc7c5bfdc20bb10c026bebd531e5865511d0bce
bisasamii <sami.soliman912@gmail.com>
2024-12-14 00:17:41 +0100

    Added more Files

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,7 +1,6 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
@@ -17,25 +16,28 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('my_bot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
+
+    # Convert xacro file to robot description XML string
+    robot_description_config = xacro.process_file(xacro_file).toxml()
+
+    # Create the robot_state_publisher node
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[{
+            'robot_description': robot_description_config,
+            'use_sim_time': use_sim_time
+        }]
     )
-
 
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
-
+            description='Use sim time if true'
+        ),
         node_robot_state_publisher
     ])
